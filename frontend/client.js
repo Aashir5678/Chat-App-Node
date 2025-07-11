e30b228b0102b0c515c4d1a3611c5c89879717c9
Aashir <48567707+Aashir5678@users.noreply.github.com>
2025-07-11 00:11:21 -0500

    first commit

---
+++ frontend/client.js
@@ -0,0 +1,52 @@
+
+
+// DISABLE FIREWALL FOR PRIVATE NETWORKS
+
+const socket = io()
+
+const inputBox = document.getElementById('message-input')
+const form = document.getElementById('send-container')
+
+let username = null
+let userMessages = {}
+
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (inputBox.value) {
+        if (username == null) {
+            socket.emit('username', inputBox.value)
+
+            username = inputBox.value
+            userMessages[username] = []
+        }
+
+        else {
+            socket.emit('message', inputBox.value)
+            console.log(username + ': ' + inputBox.value)
+        }
+
+        inputBox.value = ''
+        
+
+    }
+})
+
+socket.on('all messages', messages => {
+    userMessages = messages
+    console.log(messages)
+})
+
+socket.on('message', message => {
+    let user = message['username']
+    
+    userMessages[user].push(message['message'])
+    console.log(user + ': ' + message['message'])
+})
+
+socket.on('new connection', user => {
+    userMessages[user] = []
+    console.log('New connection: ' + user)
+})
+
---
+++ server.js
@@ -0,0 +1,57 @@
+
+
+// DISABLE FIREWALL FOR PRIVATE NETWORKS
+
+
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io') 
+const io = new Server(server)
+const port = 3000
+let users = {}
+let userMessages = {}
+
+app.use(express.static(__dirname + '/frontend'))
+
+
+io.on('connection', socket => {
+    socket.emit('all messages', userMessages)
+    
+    socket.on('disconnect', () => {
+        if (socket.id in users) {
+            socket.broadcast.emit('message', users[socket.id] + " has disconnected.")
+            console.log(users[socket.id] + " has disconnected.")
+            delete userMessages[users[socket.id]]
+            delete users[socket.id]
+
+        }
+
+        else {
+            socket.broadcast.emit('message', "Unknown user has disconnected.")
+            console.log("Unknown user has disconnected.")
+        }
+        
+    })
+
+    socket.on('message', mes => {
+        console.log(users[socket.id] + ": " + mes)
+        
+        let message_obj = {'username': users[socket.id], 'message': mes}
+        socket.broadcast.emit('message', message_obj)
+
+        userMessages[users[socket.id]].push(mes)
+    })
+
+    socket.on('username', username => {
+        console.log('New connection: ' + username)
+        socket.broadcast.emit('new connection', username)
+        users[socket.id] = username
+        userMessages[username] = [];
+    })
+})
+
+server.listen(port, () => {
+    console.log('listening on http://localhost:3000...')
+})
